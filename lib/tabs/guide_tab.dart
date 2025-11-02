8941e436acfa5f0231708db6284620b413ea486b
doaa153 <dm1959@fayoum.edu.eg>
2025-11-02 15:16:00 +0200

    Edited guide tab and Added Custom Container

--- lib/tabs/guide_tab.dart
+++ lib/tabs/guide_tab.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:mus3if/data/dummy_data.dart';
-import 'package:mus3if/widgets/guide_card.dart';
-import '../screens/guide_detail_screen.dart';
 import '../widgets/appbar_widget.dart';
+import '../widgets/custom_container_widget.dart';
 
 class GuideTab extends StatelessWidget {
   const GuideTab({super.key});
@@ -12,48 +10,17 @@ class GuideTab extends StatelessWidget {
     return Scaffold(
       backgroundColor: Color(0xFFF8FAFC),
       appBar: AppBarWidget(),
-      body: Padding(
-        padding: EdgeInsets.all(16.0),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(16),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            SizedBox(height: 16),
-            Text(
-              "Emergency Guides",
-              style: TextStyle(
-                fontSize: 28,
-                fontWeight: FontWeight.bold,
-                color: Color(0xFF1E293B),
-              ),
-            ),
-            SizedBox(height: 8),
-            Text(
-              "Learn how to handle emergency situations",
-              style: TextStyle(fontSize: 16, color: Color(0xFF64748B)),
-            ),
-            SizedBox(height: 24),
-            Expanded(
-              child: ListView.builder(
-                itemCount: guides.length,
-                itemBuilder: (context, index) {
-                  final guide = guides[index];
-                  return GuideCard(
-                    guide: guide,
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => GuideDetailScreen(guide: guide),
-                        ),
-                      );
-                    },
-                  );
-                },
-              ),
-            ),
+            SizedBox(height: 16,),
+            CustomContainerWidget(),
           ],
         ),
       ),
     );
   }
 }
+

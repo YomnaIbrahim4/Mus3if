682ad2bb4a55dd8abb36a1af89b7686f46c8d97f
Faten Hesham <fh1316@fayoum.edu.eg>
2025-11-03 21:37:55 +0200

    Fix merge conflict and update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,11 +1,8 @@
 plugins {
     id("com.android.application")
-<<<<<<< HEAD
     // START: FlutterFire Configuration
     id("com.google.gms.google-services")
     // END: FlutterFire Configuration
-=======
->>>>>>> 4f4eee8 (guide and profile tabs)
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
